36c11944932b58c520c2738215e14f0367a13eef
ghhccghk <2137610394@qq.com>
2026-05-27 21:16:44 +0800

    Enhance platform support and cryptographic functions: add JS and Wasm platform implementations, introduce SHA-1 and inflate functions, and refine existing cryptographic methods.

--- shared/src/jsMain/kotlin/com/ghhccghk/multiplatform/kugouapi/core/Platform.js.kt
+++ shared/src/jsMain/kotlin/com/ghhccghk/multiplatform/kugouapi/core/Platform.js.kt
@@ -1,2 +1,11 @@
 package com.ghhccghk.multiplatform.kugouapi.core
 
+import kotlin.js.Date
+
+class JsPlatform : Platform {
+    override val name: String = "JS"
+}
+
+actual fun getPlatform(): Platform = JsPlatform()
+
+actual fun currentTimeMillis(): Long = Date.now().toLong()
